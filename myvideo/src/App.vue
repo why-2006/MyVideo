7803454206e0d991e457fefee7fb094347bb1908
why-2006 <1639826156@qq.com>
2026-03-19 14:52:59 +0800

    layout引入

---
+++ myvideo/src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <BasicLayout />
+</template>
+<script setup lang="ts">
+import BasicLayout from "../Layout/BasicLayout.vue";
+</script>
---
+++ myvideo/vite.config.ts
@@ -0,0 +1,17 @@
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false, // css in js
+        }),
+      ],
+    }),
+  ],
+});
